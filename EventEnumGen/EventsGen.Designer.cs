9636df03e81c73dc98d37001b4be801ee6493b3c
Chameleon <abc@mail.com>
2011-03-27 09:04:10 -0400

    Script that generates eEvents_FrameXML enum

---
+++ EventEnumGen/EventsGen.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class EventsGen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Input = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Input
+            // 
+            this.Input.Location = new System.Drawing.Point(12, 12);
+            this.Input.Multiline = true;
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(657, 241);
+            this.Input.TabIndex = 0;
+            this.Input.TextChanged += new System.EventHandler(this.Input_TextChanged);
+            // 
+            // EventsGen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(681, 265);
+            this.Controls.Add(this.Input);
+            this.Name = "EventsGen";
+            this.Text = "EventEnumGen";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Input;
+    }
+}
+
---
+++ EventEnumGen/EventsGen.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+    public partial class EventsGen : Form
+    {
+        public EventsGen()
+        {
+            InitializeComponent();
+        }
+
+        // This method generates eEvents_FrameXML enum from hex rays output of the function 
+        // where the strings from that enum are used to initialize a huge array.
+        private void Input_TextChanged(object sender, EventArgs e)
+        {
+            StreamWriter outp = File.CreateText("eEvents_FrameXML.h");
+            outp.WriteLine("enum eEvents_FrameXML");
+            outp.WriteLine("{");
+            Regex r = new Regex(".*dword_([0-9A-Fa-f]+)(?=/[.+/])? =.*\"(.+)\"");
+            string[] lines = Input.Lines;
+            Array.Sort(lines);
+            string lastEvent = "";
+            Dictionary<string, int> occurences = new Dictionary<string, int>();
+            int firstOffset = 0;
+            foreach (string line in lines)
+            {
+                Match m = r.Match(line);
+				if (!m.Success)
+				{
+					continue;
+				}
+                int offset = Convert.ToInt32(m.Groups[1].Captures[0].Value, 16);
+                string eventName = m.Groups[2].Captures[0].Value;
+                if(firstOffset == 0)
+                {
+                    firstOffset = offset;
+                }
+                offset -= firstOffset;
+                offset /= 4;
+                if (occurences.ContainsKey(eventName))
+                {
+                    occurences[eventName] += 1;
+                }
+                else
+                {
+                    occurences[eventName] = 1;
+                }
+                lastEvent = eventName;
+                outp.WriteLine("    EVENT_" + eventName + (occurences[eventName] > 1 ? ("_" + occurences[eventName]) : "") +
+                    " = 0x{0:X},", offset);
+            }
+            outp.WriteLine("}");
+            outp.Close();
+        }
+    }
+}
