a8586ed5fba8487e759461dd943e5d6a06432007
Chameleon <c-n@bk.ru>
2011-07-12 00:33:28 -0400

    HexDecClipboard: tool that converts content of windows clipboard to/from hex.

---
+++ HexDecClipboard/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace HexDecClipboard
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            try
+            {
+                bool toDec = false;
+                string text = System.Windows.Forms.Clipboard.GetText();
+                if(String.IsNullOrEmpty(text))
+                {
+                    return;
+                }
+                if(args.Length > 0 && args[0] == "h")
+                {
+                    toDec = true;
+                    text = text.Replace("0x", "");
+                }
+                else if (text.StartsWith("0x"))
+                {
+                    toDec = true;
+                    text = text.Substring(2);
+                }
+                else if (Regex.IsMatch(text, @"[^\-0-9]"))
+                {
+                    toDec = true;   // assume contains A-F
+                }
+                text = text.ToLower();
+                if (!Regex.IsMatch(text, "-?[0-9a-fA-F]+"))
+                {
+                    return; // invalid format
+                }
+                long value = toDec ? Convert.ToInt64(text, 16) : Int64.Parse(text);
+                System.Windows.Forms.Clipboard.SetText(string.Format(toDec ? "{0}" : "{0:X}", value));
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+}
---
+++ HexDecClipboard/README.txt
@@ -0,0 +1,4 @@
+A simple tool that replaces content of windows clipboard with its current value converted to/from hex.
+It is expected to be run using a keyboard shortcut. No GUI is displayed.
+In most cases it should be able to figure out which way the conversion should be.
+If you want to specify that input value is in hex even though it contains only digits, add console argument h.
